0f097a82fa8b1bc0e770a5c68fff43b1108af0df
Daniel Ruiz <danielruimar@gmail.com>
2023-02-24 11:05:32 +0100

    add component Book and styles

---
+++ src/components/Book/Book.js
@@ -0,0 +1,18 @@
+import './Book.css';
+
+const Book = ({book})=>{
+
+    const authors = [book.volumeInfo.authors];
+    return(
+    <tr key={book.id}>
+        <td>{book.volumeInfo.authors ? authors.map(author=> author.toString()) : 'Sin autores'}</td>
+        <td>{book.volumeInfo.title}</td>
+        <td>{book.volumeInfo.description || 'Sin descripción'}</td>
+        <td>{book.volumeInfo.imageLinks ? <img alt='Sin imagen' src={book.volumeInfo.imageLinks.thumbnail} /> : 'No hay imagen'}</td>
+    </tr>
+
+
+    )
+}
+
+export default Book;
--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,6 +1,7 @@
 import './BookList.css'
 import React from 'react'
 import { useDebounce } from 'use-debounce';
+import Book from '../Book/Book';
 
 const API_URL = 'https://www.googleapis.com/books/v1/volumes?q='
 
@@ -10,7 +11,7 @@ const BookList = () => {
     const [filter, setFilter] = React.useState('');
     const [visible, setVisible] = React.useState(false);
 
-    const [filterWithTime] = useDebounce(filter, 1000);
+    const [filterWithTime] = useDebounce(filter, 500);
 
     const callApi = () => {
 
@@ -32,36 +33,28 @@ const BookList = () => {
     return (
 
         <div className='bookslist'>
-            <div>
+            <div className='bookslist__container'>
                 <p className='bookslist__text'>Buscador de libros</p>
                 <input className='bookslist__input' type='text' onChange={(event) => setFilter(event.target.value)} value={filter} />
             </div>
 
-            {(filterWithTime.length < 3) && <p>Introduce al menos tres caracteres...</p>}
+            {(filterWithTime.length < 3) && <p className='bookslist__nocharacters'>Introduce al menos tres caracteres...</p>}
             
-            {((visible === false) && (filterWithTime.length >= 3)) && <p>No hay libros</p>}
+            {((visible === false) && (filterWithTime.length >= 3)) && <p className='bookslist__nobooks'>No hay libros</p>}
 
             {((visible === true) && (filterWithTime.length >= 3)) && <table className='bookslist__table'>
                     <thead>
                         <tr>
-                            <th>Autores</th>
-                            <th>Título</th>
-                            <th>Descripción</th>
-                            <th>Portada</th>
+                            <th>AUTORES</th>
+                            <th>TÍTULO</th>
+                            <th>DESCRIPCIÓN</th>
+                            <th>PORTADA</th>
                         </tr>
                     </thead>
                     <tbody>
 
-                        {booksList.map(book => (
-
-                            <tr key={book.id}>
-                                <td>{book.volumeInfo.authors || 'Sin autores'}</td>
-                                <td>{book.volumeInfo.title}</td>
-                                <td>{book.volumeInfo.description || 'Sin descripción'}</td>
-                                <td>{book.volumeInfo.imageLinks ? <img alt='Sin imagen' src={book.volumeInfo.imageLinks.thumbnail} /> : 'No hay imagen'}</td>
-                            </tr>
-                        ))}
-
+                        {booksList.map(book =><Book key= {book.id} book = {book}/>)}
+                        
                     </tbody>
                 </table>}
         </div>
