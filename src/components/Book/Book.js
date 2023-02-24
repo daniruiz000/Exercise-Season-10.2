8b2076720110d70764e4ef526aadd5c6ce148f12
Daniel Ruiz <danielruimar@gmail.com>
2023-02-24 12:14:20 +0100

    add styles and fix problems authors

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -5,10 +5,10 @@ const Book = ({book})=>{
     const authors = [book.volumeInfo.authors];
     return(
     <tr key={book.id}>
-        <td>{book.volumeInfo.authors ? authors.map(author=> author.toString()) : 'Sin autores'}</td>
+        <td>{book.volumeInfo.authors ? authors.map(author=> author.toString().replaceAll(',', ', ')) : 'Sin autores'}</td>
         <td>{book.volumeInfo.title}</td>
         <td>{book.volumeInfo.description || 'Sin descripción'}</td>
-        <td>{book.volumeInfo.imageLinks ? <img alt='Sin imagen' src={book.volumeInfo.imageLinks.thumbnail} /> : 'No hay imagen'}</td>
+        <td>{book.volumeInfo.imageLinks ? <img alt='img book' src={book.volumeInfo.imageLinks.thumbnail} /> : 'No hay imagen'}</td>
     </tr>
 
 
